04c92fc7410428692d013d1b95fd409ceca1627e
huhuthuhut <40786679+huhuthuhut@users.noreply.github.com>
2019-06-24 10:34:10 +0900

    로그인, 회원가입 매핑

--- src/main/java/com/good/youditor/AccountsController.java
+++ src/main/java/com/good/youditor/AccountsController.java
@@ -1,12 +1,14 @@
 package com.good.youditor;
 
 import java.util.List;
+import java.util.Locale;
 
 import javax.inject.Inject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.good.dto.AccountsVO;
 import com.good.service.AccountsService;
@@ -26,6 +28,22 @@ public class AccountsController {
 	public void show(Model model) throws Exception {
 		List<AccountsVO> list = service.selectAccounts();
 		model.addAttribute("list",list);
+	}
+	
+	@RequestMapping(value = "/join.do", method=RequestMethod.GET)
+	public ModelAndView join(Locale locale, Model model) throws Exception {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("accounts/join");
+		System.out.println("String join open");
+		return mav;	
+	}
+	
+	@RequestMapping(value = "/login.do", method=RequestMethod.GET)
+	public ModelAndView login(Locale locale, Model model) throws Exception {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("accounts/login");
+		System.out.println("String login open");
+		return mav;	
 	}
 
 
