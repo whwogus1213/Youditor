de8d69358b27ac75165cfe0f813d1365f463d6c3
wlghdwlsg <wlghdwlsg@gmail.com>
2019-06-26 18:26:41 +0900

    Update RecruitBoardController.java

--- src/main/java/com/good/youditor/RecruitBoardController.java
+++ src/main/java/com/good/youditor/RecruitBoardController.java
@@ -13,7 +13,6 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.good.dto.AccountsVO;
 import com.good.dto.RecruitBoardVO;
-import com.good.dto.RecruitBoardVO;
 import com.good.service.RecruitBoardService;
 
 @Controller
