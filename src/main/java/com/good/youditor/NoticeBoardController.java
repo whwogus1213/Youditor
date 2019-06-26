3a6b8654b35659df6e677de3a717336a51b14c4b
huhuthuhut <40786679+huhuthuhut@users.noreply.github.com>
2019-06-26 17:31:20 +0900

    Revert "Master Commit"
    This reverts commit 95617c73ba9a02de6461568991e978d6ded6e6fc, reversing
    changes made to 5cc00e051ca1209300ca27f3f4561a8149cf0e4b.

--- src/main/java/com/good/youditor/NoticeBoardController.java
+++ src/main/java/com/good/youditor/NoticeBoardController.java
@@ -1,60 +1,23 @@
 package com.good.youditor;
 
 import java.util.List;
-
-import javax.inject.Inject;
+import java.util.Locale;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
-import com.good.dto.AccountsVO;
-import com.good.dto.NoticeBoardVO;
-import com.good.dto.NoticeBoardVO;
-import com.good.service.NoticeBoardService;
 
 @Controller
 @RequestMapping("/noticeboard")
 public class NoticeBoardController {
-
-	@Inject
-	NoticeBoardService noticeBoardService;
-
 	@RequestMapping(value = "/noticeBoardList")
-	public ModelAndView list() throws Exception {
-		List<NoticeBoardVO> list = noticeBoardService.listAll();
-
+	public ModelAndView listdo(Locale locale, Model model) throws Exception {
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("noticeboard/noticeBoardList");
-		System.out.println("NoticeBoardController NoticeBoardList open");
-		mav.addObject("NoticeBoardList", list);
-		return mav;
-	}
-
-	// 게시물 상세정보
-	@RequestMapping(value = "/noticeBoardView", method = RequestMethod.GET)
-	public ModelAndView view(@RequestParam("boardId") int boardId) throws Exception {
-		System.out.println("*************************************************");
-		NoticeBoardVO row = noticeBoardService.view(boardId);
-
-		ModelAndView mav = new ModelAndView();
-		mav.setViewName("noticeboard/noticeBoardView");
-		mav.addObject("row", row);
-		System.out.println("NoticeBoardController noticeBoardView open");
-		return mav;
-	}
-
-	// 게시물 쓰기
-	@RequestMapping(value = "/write.do", method = RequestMethod.GET)
-	public ModelAndView writedo() throws Exception {
-		System.out.println("*************************************************");
-
-		ModelAndView mav = new ModelAndView();
-		mav.setViewName("noticeboard/boardWrite");
-		System.out.println("NoticeBoardController boardWrite open");
-		return mav;
+		
+		System.out.println("String notice-main open");
+		return mav;	
 	}
 }
